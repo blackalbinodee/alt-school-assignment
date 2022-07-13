b099a1036b2d6ce9e1e9cef6933203fbafa55615
Dumebi Ofili <blackalbinodee@gmail.com>
2022-07-13 21:00:49 +0000

    assignment 1 and 2 1. Implement the sumOfNumber function 2.Implement the countEvenNumbersFunction

--- src/assignment.js
+++ src/assignment.js
@@ -13,10 +13,16 @@ const assignment = {};
  * @param {Array} arrayOfNumbers the array of numbers to sum
  * @returns number the sum of the numbers
  */
+
 function sumOfNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+  let sumOfNumbers = 0; 
+  for(let i = 0; i < arrayOfNumbers.length; i++) {
+  sumOfNumbers += arrayOfNumbers[i];
+  }
+  return sumOfNumbers;
+  return arrayOfNumbers.length;
 }
-// assignment.sumOfNumbers = sumOfNumbers;
+assignment.sumOfNumbers = sumOfNumbers;
 
 /**
  * Challenge - 2
@@ -28,15 +34,21 @@ function sumOfNumbers(arrayOfNumbers) {
  * @returns number the count of even numbers
  */
 function countEvenNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+let countEvenNumbers = 0;
+  for( let i = 0; i<arrayOfNumbers.length; i++){
+    if (arrayOfNumbers[i] %2 == 0){
+      countEvenNumbers++;
+    }
+  }
+  return countEvenNumbers;
 }
-// assignment.countEvenNumbers = countEvenNumbers;
+assignment.countEvenNumbers = countEvenNumbers;
 
 /**
  * Challenge - 3
  * 
- * Given an array of numbers representing temperatures in Celsius,
- * convert each number to Fahrenheit and return a new array containing the converted
+ * Given an array o f numbers representing temperatures in Celsius,
+ * convert each number to Fahrenheit and return a new array containi ng the converted
  * temperatures in Fahrenheit. Decimal figures in the converted values in Fahrenheit should be removed.
  * E.g 51.21 should just be 51 (hint: Math.trunc(...) function)
  * If you did Challenge - 3, remove the comment in the line just after this function
@@ -47,9 +59,13 @@ function countEvenNumbers(arrayOfNumbers) {
  * @returns Array the converted temperatures in Fahrenheit
  */
 function celsiusToFahrenheit(arrayOfNumbers) {
-    return arrayOfNumbers;
+  return arrayOfNumbers.map(
+    function(temp) {
+      return celsiusToFahrenheit = Math.trunc((temp-32) * 5 / 9);
+    }
+  );  
 }
-// assignment.celsiusToFahrenheit = celsiusToFahrenheit;
+//assignment.celsiusToFahrenheit = celsiusToFahrenheit;
 
 
 // ========================
